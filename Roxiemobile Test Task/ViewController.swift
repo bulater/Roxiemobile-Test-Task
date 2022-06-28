9bd4977fa689610395efce308d75f9895a118fb8
Bulat Sagdiev <com.BulatSagdiev>
2022-06-28 20:36:44 +0300

    Initial Commit

---
+++ Roxiemobile Test Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Roxiemobile Test Task
+//
+//  Created by Булат Сагдиев on 28.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
