9f29c482c2a03123ae8059ea9c96da9fc5b0b004
Bulat Sagdiev <com.BulatSagdiev>
2022-06-29 08:10:48 +0300

    create networking
    added:
    - networkManager class + fetchData method
    - activeOrders Entity
    - activeOrders viewModel

---
+++ Roxiemobile Test Task/Modules/ActiveOrdersList/Model/ActiveOrdersViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  ActiveOrdersViewModel.swift
+//  Roxiemobile Test Task
+//
+//  Created by Булат Сагдиев on 29.06.2022.
+//
+
+import Foundation
+
+struct ActiveOrdersViewModel {
+    var startCity: String
+    var startAdress: String
+    var endCity: String
+    var endAddress: String
+    var orderTime: Date
+    var amount: String
+    var currency: String
+
+    init(order: ActiveOrders) {
+        self.startCity = order.startAddress.city
+        self.startAdress = order.startAddress.address
+        self.endCity = order.endAddress.address
+        self.endAddress = order.endAddress.address
+        self.orderTime = order.orderTime
+        self.amount = String(order.price.amount)
+        self.currency = order.price.currency
+    }
+}
---
+++ Roxiemobile Test Task/Services/NetworkManager.swift
@@ -0,0 +1,58 @@
+//
+//  NetworkManager.swift
+//  Roxiemobile Test Task
+//
+//  Created by Булат Сагдиев on 29.06.2022.
+//
+
+import UIKit
+
+enum NetworkError: Error {
+    case invalidURL
+    case noData
+    case decodingError
+}
+
+class NetworkManager {
+    // MARK: - Private Properties
+
+    static let shared = NetworkManager()
+
+    private let session = URLSession.shared
+    private let decoder = JSONDecoder()
+
+    private enum Path {
+        static let activeOrders = "https://www.roxiemobile.ru/careers/test/orders.json"
+    }
+
+    // MARK: - Public Methods
+
+    func fetchMarvelHeroesData(completion: @escaping (Result<[ActiveOrders], NetworkError>) -> Void) {
+        guard let url = URL(string: Path.activeOrders) else {
+            completion(.failure(.invalidURL))
+            return
+        }
+
+        let task = session.dataTask(with: url) { data, _, error in
+            guard let parseredData = data else {
+                completion(.failure(.noData))
+                return
+            }
+
+            do {
+                let decodedData = try self.decoder.decode([ActiveOrders].self, from: parseredData)
+                completion(.success(decodedData))
+            } catch {
+                print(error.localizedDescription)
+                completion(.failure(.decodingError))
+            }
+        }
+
+        task.resume()
+    }
+}
+
+class ImageCache {
+    static let shared = NSCache<NSString, UIImage>()
+    private init() {}
+}
